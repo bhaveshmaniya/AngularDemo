519d827cb7816d396b43d12611f463e02ac35079
Bhavesh Maniya <bhaveshmaniya@gmail.com>
2018-06-10 23:10:42 +0530

    Angular component input property change using Property Setter

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -6,20 +6,27 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit, OnChanges {
-  @Input() employee: Employee;
+export class DisplayEmployeeComponent implements OnInit {
+  // Private backing field for the property
+  private _employee: Employee;
+
+  // This property setter is called anytime the input property changes
+  // Notice the code here logs the previous and current employee names
+  @Input()
+  set employee(val: Employee) {
+    console.log('Previous : ' + (this._employee ? this._employee.name : 'NULL'));
+    console.log('Current : ' + val.name);
+    this._employee = val;
+  }
+
+  // This getter is called when reading and displaying data
+  get employee(): Employee {
+    return this._employee;
+  }
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    const previousEmployee = <Employee>changes.employee.previousValue;
-    const currentEmployee = <Employee>changes.employee.currentValue;
-
-    console.log('Previous : ' + (previousEmployee ? previousEmployee.name : 'NULL'));
-    console.log('Current : ' + currentEmployee.name);
   }
 
 }
