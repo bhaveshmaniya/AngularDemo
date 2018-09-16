dcaca50d1377c714683dc1fa3ae85b280a3d1758
Bhavesh Maniya <bhaveshmaniya@gmailcom>
2018-09-16 10:45:25 +0530

    Used HttpClient service with delete method to delete the resource

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -40,7 +40,10 @@ export class DisplayEmployeeComponent implements OnInit {
   }
 
   deleteEmployee() {
-    this._employeeService.deleteEmployee(this.employee.id);
+    this._employeeService.deleteEmployee(this.employee.id).subscribe(
+      () => { console.log(`Employee with Id=${this.employee.id} deleted`); },
+      (err) => { console.log(err); }
+    );
     // Raise notifyDelete event, so  the ListEemployeesComponent can delete the same employee from it's filtered list array
     this.notifyDelete.emit(this.employee.id);
   }
--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -16,59 +16,48 @@ export class EmployeeService {
     constructor(private _httpClient: HttpClient) {
     }
 
-    private listEmployees: Employee[] = [
-        {
-            id: 1,
-            name: 'Mark',
-            gender: 'Male',
-            contactPreference: 'Email',
-            email: 'mark@pragimtech.com',
-            dateOfBirth: new Date('10/25/1988'),
-            department: '3',
-            isActive: true,
-            photoPath: 'assets/images/mark.png'
-        },
-        {
-            id: 2,
-            name: 'Mary',
-            gender: 'Female',
-            contactPreference: 'Phone',
-            phoneNumber: 2345978640,
-            dateOfBirth: new Date('11/20/1979'),
-            department: '2',
-            isActive: true,
-            photoPath: 'assets/images/mary.png'
-        },
-        {
-            id: 3,
-            name: 'John',
-            gender: 'Male',
-            contactPreference: 'Phone',
-            phoneNumber: 5432978640,
-            dateOfBirth: new Date('3/25/1976'),
-            department: '3',
-            isActive: false,
-            photoPath: 'assets/images/john.png'
-        },
-    ];
+    // private listEmployees: Employee[] = [
+    //     {
+    //         id: 1,
+    //         name: 'Mark',
+    //         gender: 'Male',
+    //         contactPreference: 'Email',
+    //         email: 'mark@pragimtech.com',
+    //         dateOfBirth: new Date('10/25/1988'),
+    //         department: '3',
+    //         isActive: true,
+    //         photoPath: 'assets/images/mark.png'
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Mary',
+    //         gender: 'Female',
+    //         contactPreference: 'Phone',
+    //         phoneNumber: 2345978640,
+    //         dateOfBirth: new Date('11/20/1979'),
+    //         department: '2',
+    //         isActive: true,
+    //         photoPath: 'assets/images/mary.png'
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'John',
+    //         gender: 'Male',
+    //         contactPreference: 'Phone',
+    //         phoneNumber: 5432978640,
+    //         dateOfBirth: new Date('3/25/1976'),
+    //         department: '3',
+    //         isActive: false,
+    //         photoPath: 'assets/images/john.png'
+    //     },
+    // ];
 
     getEmployees(): Observable<Employee[]> {
         //return Observable.of(this.listEmployees).delay(2000);
         //return this._httpClient.get<Employee[]>('this.baseUrl').delay(2000);
-        return this._httpClient.get<Employee[]>(this.baseUrl)
+        return this._httpClient.get<Employee[]>(this.baseUrl).delay(2000)   // Added delay of 2 seconds
             //.catch(this.handleError);
             .pipe(catchError(this.handleError));
-    }
-
-    private handleError(errorResponse: HttpErrorResponse) {
-        if (errorResponse instanceof ErrorEvent) {
-            console.error('Client Side Error :', errorResponse.error.message);
-        } else {
-            console.error('Server Side Error :', errorResponse);
-        }
-
-        // return an observable with a meaningful error message to the end user
-        return new ErrorObservable('There is a problem with the service.We are notified & working on it. Please try again later.');
     }
 
     getEmployee(id: number): Observable<Employee> {
@@ -94,10 +83,19 @@ export class EmployeeService {
         }).pipe(catchError(this.handleError));
     }
 
-    deleteEmployee(id: number) {
-        var foundIndex = this.listEmployees.findIndex(e => e.id === id);
-        if (foundIndex !== -1) {
-            this.listEmployees.splice(foundIndex, 1);
+    deleteEmployee(id: number): Observable<void> {
+        return this._httpClient.delete<void>(`${this.baseUrl}/${id}`)
+            .pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorResponse: HttpErrorResponse) {
+        if (errorResponse instanceof ErrorEvent) {
+            console.error('Client Side Error :', errorResponse.error.message);
+        } else {
+            console.error('Server Side Error :', errorResponse);
         }
+
+        // return an observable with a meaningful error message to the end user
+        return new ErrorObservable('There is a problem with the service.We are notified & working on it. Please try again later.');
     }
 }
